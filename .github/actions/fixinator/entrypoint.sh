de94296c396ec9e7ed8607173212ad35fd21acbc
Michael Born <michael@ortussolutions.com>
2021-09-23 17:16:19 -0400

    🐛 FIX: Drop api_url for now

--- .github/actions/fixinator/entrypoint.sh
+++ .github/actions/fixinator/entrypoint.sh
@@ -16,9 +16,9 @@ FIXINATOR_FAILONISSUES=$9
 # Since Fixinator uses underscores in its module configuration, we can't do this with a simple env var substitution.
 ###
 box config set modules.fixinator.api_key=$FIXINATOR_API_KEY
-if [[ -n $API_URL ]]; then
-    box config set modules.fixinator.api_url=$API_URL
-fi
+# if [[ -n $API_URL ]]; then
+#     box config set modules.fixinator.api_url=$API_URL
+# fi
 
 ###
 # RUN IT
